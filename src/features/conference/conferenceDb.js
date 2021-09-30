3337825dd2e5451f39a26119bcece5309d94196d
AndreiIlie1397 <andrei.ilie1397@gmail.com>
2021-09-30 16:41:33 +0300

    Conference add/edit page

--- src/features/conference/conferenceDb.js
+++ src/features/conference/conferenceDb.js
@@ -1,15 +1,17 @@
 const { SQLDataSource } = require('../../utils/sqlDataSource')
 
+const conferenceColumns = ['Id', 'Name', 'ConferenceTypeId', 'LocationId', 'CategoryId', 'StartDate', 'EndDate']
 class ConferenceDb extends SQLDataSource {
   generateWhereClause(queryBuilder, filters = {}) {
-    const { startDate, endDate } = filters
+    const { startDate, endDate, organizerEmail } = filters
     if (startDate) queryBuilder.andWhere('StartDate', '>=', startDate)
     if (endDate) queryBuilder.andWhere('EndDate', '<=', endDate)
+    if (organizerEmail) queryBuilder.andWhere('OrganizerEmail', organizerEmail)
   }
   async getConferenceList(pager, filters) {
     const { page, pageSize } = pager
     const values = await this.knex
-      .select('Id', 'Name', 'ConferenceTypeId', 'LocationId', 'CategoryId', 'StartDate', 'EndDate')
+      .select(...conferenceColumns)
       .from('Conference')
       .modify(this.generateWhereClause, filters)
       .orderBy('Id')
@@ -19,6 +21,15 @@ class ConferenceDb extends SQLDataSource {
   }
   async getConferenceListTotalCount(filters = {}) {
     return await this.knex('Conference').count('Id', { as: 'TotalCount' }).modify(this.generateWhereClause, filters).first()
+  }
+
+  async getConferenceById(id) {
+    const result = await this.knex()
+      .select(...conferenceColumns)
+      .from('Conference')
+      .where('Id', id)
+      .first()
+    return result
   }
 }
 module.exports = ConferenceDb
